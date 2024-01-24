88a275e6f710f63a137bb1ae4c33415ef7f9f9b1
yuriistadnikov <y.stadnikov@gmail.com>
2024-01-24 20:08:38 +0200

    Removed unused imports

--- backend/src/main/java/com/yuriist/Main.java
+++ backend/src/main/java/com/yuriist/Main.java
@@ -1,13 +1,11 @@
 package com.yuriist;
 
 import com.github.javafaker.Faker;
-import com.github.javafaker.Name;
 import com.yuriist.customer.Customer;
 import com.yuriist.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Random;
