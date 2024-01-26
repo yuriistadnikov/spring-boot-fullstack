7d51614022f1757d992d2fbb001289b7ef2f58b5
yuriistadnikov <y.stadnikov@gmail.com>
2024-01-26 21:33:58 +0200

    CD Workflow with new controller

---
+++ backend/src/main/java/com/yuriist/PingPongController.java
@@ -0,0 +1,15 @@
+package com.yuriist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+
+    record PingPong(String result){}
+}
